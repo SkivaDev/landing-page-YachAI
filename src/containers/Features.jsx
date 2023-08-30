e8303e981513fcd0477de8c48710f80fb1c88606
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-08-30 10:04:43 -0500

    feat(containers): :lipstick: add HeroSection, and Features a styles to limit the width

--- src/containers/Features.jsx
+++ src/containers/Features.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { features } from '../data/features.json'
-import Card from '../components/Card'
+import React from "react";
+import { features } from "../data/features.json";
+import Card from "../components/Card";
 
 function Features() {
   return (
     <div className='w-full px-[90px] py-[58px] bg-white2'>
       <h3 className='text-[18px] text-center mb-[64px]'>Usa Inteligencia artificial para mejorar tu aprendizaje.</h3>
-      <div className='flex justify-between items-start mb-[64px]'>
+      <div className='flex justify-between items-start mb-[64px] max-w-[1240px] mx-auto'>
         {features.map((feature, i) => (
           <Card key={i} {...feature}/>
         ))}
       </div>
       
     </div>
-  )
+  );
 }
 
-export default Features
+export default Features;
--- src/containers/HeroSection.jsx
+++ src/containers/HeroSection.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
-import laptop_img from '../assets/images/yachai_banner.png'
+import React from "react";
+import laptop_img from "../assets/images/yachai_banner.png";
 
 function HeroSection() {
   return (
-    <div className='w-full pl-[32px] mt-[132px] mb-[145px] flex bg-red-100 gap-[81px]'>
-      <div className='flex-1 '>
-        <h1 className='text-[46px] mt-[27px] text-smooth_black leading-title_hero'>Ya no abandones <br /> los estudios por <br />falta de tutor</h1>
-        <h3 className='text-[20px] mt-[27px]'>Servicio de tutoria académica personalizada.</h3>
-        <p className='text-[18px] text-smooth_black mt-[21px] max-w-[509px]'>Convierte con facilidad cualquier video, documento o URL en un curso interactivo con asistente inteligente. Perfecto para empresas, profesionales y estudiantes.</p>
-        <span className='block text-[18px] text-smooth_purple mt-[11px]'>Potenciado por Inteligencia Artificial</span>
-        <div className='flex items-center gap-[40px] mt-[31px]'>
-          <button className='py-[9px] px-[11px] bg-light_purple rounded-xl text-white'>Crea un tutor gratis</button>
-          <button>Saber más <span>→</span></button>
+    <div className="w-full pl-[32px] mt-[132px] mb-[145px] bg-white gap-[81px]">
+      <div className="max-w-[1240px] mx-auto flex">
+        <div className="flex-1 ">
+          <h1 className="text-[46px] mt-[27px] text-smooth_black leading-title_hero">
+            Ya no abandones <br /> los estudios por <br />
+            falta de tutor
+          </h1>
+          <h3 className="text-[20px] mt-[27px]">
+            Servicio de tutoria académica personalizada.
+          </h3>
+          <p className="text-[18px] text-smooth_black mt-[21px] max-w-[509px]">
+            Convierte con facilidad cualquier video, documento o URL en un curso
+            interactivo con asistente inteligente. Perfecto para empresas,
+            profesionales y estudiantes.
+          </p>
+          <span className="block text-[18px] text-smooth_purple mt-[11px]">
+            Potenciado por Inteligencia Artificial
+          </span>
+          <div className="flex items-center gap-[40px] mt-[31px]">
+            <button className="py-[9px] px-[11px] bg-light_purple rounded-xl text-white">
+              Crea un tutor gratis
+            </button>
+            <button>
+              Saber más <span>→</span>
+            </button>
+          </div>
+        </div>
+        <div className="w-[570px] h-[447px]">
+          <img src={laptop_img} alt="laptop_img" className="w-full h-full" />
         </div>
       </div>
-      <div className='w-[570px] h-[447px]'>
-        <img src={laptop_img} alt="laptop_img" className='w-full h-full'/>
-      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
